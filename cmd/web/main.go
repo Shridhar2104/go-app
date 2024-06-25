cd42686e73da1e58c74bebf71f4a4add5dfcae15
Shridhar2104 <155125695+Shridhar2104@users.noreply.github.com>
2024-06-25 15:14:07 +0530

    go application

---
+++ cmd/web/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+const cssVersion = "1"
+
+type config struct {
+	port int
+	env  string
+	api  string
+	db   struct {
+		dsn string
+	}
+	stripe struct {
+		secret string
+		key    string
+	}
+}
+
+type application struct {
+	config   config
+	infolog  *log.Logger
+	errorlog *log.Logger
+
+	templateCache map[string]*template.Template
+	version       string
+}
+
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeou
